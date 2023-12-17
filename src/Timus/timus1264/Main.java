abc614f402db5b9b28bcd9bacbf5ca1892ffa2b2
alexander <advt2024@mail.ru>
2023-12-17 19:17:06 +0500

    Timus

---
+++ src/Timus/timus1264/Main.java
@@ -0,0 +1,19 @@
+package Timus.timus1264;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        int numberOfM = m + 1;
+        int result = n * numberOfM;
+
+        System.out.println(result);
+    }
+}
