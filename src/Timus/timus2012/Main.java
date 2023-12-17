3ec06103028b71327e91efe72380d79674ed0733
alexander <advt2024@mail.ru>
2023-12-17 18:56:50 +0500

    Timus

---
+++ src/Timus/timus2012/Main.java
@@ -0,0 +1,26 @@
+package Timus.timus2012;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int numberOfTasks = in.nextInt();
+
+        int difference = 12 - numberOfTasks;
+
+        int time = difference * 45;
+
+        if(time <= 240) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        out.flush();
+    }
+}
