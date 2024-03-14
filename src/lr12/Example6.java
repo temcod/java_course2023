7551b585c3b974bc391583a06aab4c8417c57232
alexander <advt2024@mail.ru>
2024-03-14 19:46:16 +0500

    lr12

--- src/lr12/Example6.java
+++ src/lr12/Example6.java
@@ -1,6 +1,6 @@
 package lr12;
 //        Напишите функцию, которая суммирует элементы в массиве целых чисел при помощи многопоточности.
-//        Количество потоков должно быть равно количеству ядер процессора
+//        Количество потоков должно быть равно количеству ядер
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
